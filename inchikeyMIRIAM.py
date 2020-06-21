6b2643cfa32aa549b83fea7a9554cb74e86be61e
mdulac <melchior.dulac@inra.fr>
2020-06-21 20:38:00 +0200

    changed the names to merge with other projects

--- inchikeyMIRIAM.py
+++ inchikeyMIRIAM.py
@@ -3,6 +3,14 @@
 import rpSBML
 import libsbml
 import logging
+import rpCache
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    #level=logging.WARNING,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%d-%m-%Y %H:%M:%S',
+)
 
 
 class inchikeyMIRIAM:
@@ -54,3 +62,13 @@ class inchikeyMIRIAM:
                     logging.warning('Cannot find the inchikey for: '+str(spe.id))
         libsbml.writeSBMLToFile(rpsbml.document, out_sbml)
         return True
+
+def main(gem_sbml,
+         out_sbml):
+    rpcache = rpCache.rpCache()
+    inchikeymiriam = rpTool.inchikeyMIRIAM()
+    inchikeymiriam.deprecatedCID_cid = rpcache.getDeprecatedCID()
+    inchikeymiriam.cid_strc = rpcache.getCIDstrc()
+    inchikeymiriam.chebi_cid = rpcache.getChebiCID()
+    inchikeymiriam.addInChiKey(gem_sbml, out_sbml)
+
